39153a0c4f82756a5fb5407b5f0363fbe48d6eab
Yauhen <stan01@tut.by>
2017-04-08 21:41:24 +0300

    SSL implementation

---
+++ src/main/java/masterSpringMvc/config/SslConfig.java
@@ -0,0 +1,39 @@
+package masterSpringMvc.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+/**
+ * Created by Aspire on 08.04.2017.
+ */
+@Configuration
+public class SslConfig {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() throws IOException {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() throws IOException {
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        connector.setPort(8443);
+        connector.setSecure(true);
+        connector.setScheme("https");
+        protocol.setSSLEnabled(true);
+        protocol.setKeyAlias("masterspringmvc");
+        protocol.setKeystorePass("password");
+        protocol.setKeyPass("password");
+        protocol.setKeystoreFile(new ClassPathResource("tomcat.keystore").getFile().getAbsolutePath());
+        protocol.setSslProtocol("TLS");
+        return connector;
+    }
+}
